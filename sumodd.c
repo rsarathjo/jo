74d9591fa4e99df9dc372d1e1a6f5d05b18b6cc5
rsarathjo <30317731+rsarathjo@users.noreply.github.com>
2017-07-23 16:09:52 +0530

    Create sumodd.c

---
+++ sumodd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+viod main()
+{
+int a,b,x1,y1;
+for(a=0;a<15;a++)
+x1+=a;
+printf("1 to 15 %d x1=%d\n",x1);
+for(b=0;b<45;b++);
+if(b%2!=0)
+y1+=y;
+printf("15 to 45  y1=%d\n",y1);
+return 0;
+}
