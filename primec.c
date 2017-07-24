b2c9a3a00f660249700f3862c829c37c8a9da43d
rsarathjo <30317731+rsarathjo@users.noreply.github.com>
2017-07-24 11:08:24 +0530

    Create primec.c

---
+++ primec.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int i,n, j=0;
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n%2==0)
+{
+j=1;
+break;
+}
+}
+if(j==0)
+{
+printf("prime");
+}
+else
+{
+printf("not prime");
+}
+}
