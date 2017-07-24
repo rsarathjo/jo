925b6742a0202ea87c60f480a3b567030f0a58b2
rsarathjo <30317731+rsarathjo@users.noreply.github.com>
2017-07-24 16:10:55 +0530

    Create generate fibabocci.c

---
+++ generate fibabocci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int i, n, t1 = 0, t2 = 1, c;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= n; ++i)
+    {
+        printf("%d, ", t1);
+      c = t1 + t2;
+        t1 = t2;
+        t2 = c;
+    }
+    return 0;
+}
