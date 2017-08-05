89aa34488321bfc663cc5d19193ed98735526f3a
rsarathjo <30317731+rsarathjo@users.noreply.github.com>
2017-08-05 16:43:35 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+   char str[1000], ch;
+   int i, frequency = 0;
+
+   printf("Enter a string: ");
+   gets(str);
+
+   printf("Enter a character : ");
+   scanf("%c",&ch);
+
+   for(i = 0; str[i] != '\0'; ++i)
+   {
+       if(ch == str[i])
+           ++frequency;
+   }
+
+   printf("Frequency of %c = %d", ch, frequency);
+
+   }
